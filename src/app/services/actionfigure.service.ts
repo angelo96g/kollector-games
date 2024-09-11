af06c0fc697c18a6d4030d25e5c5687d19d564cd
angelo96g <angelo96g@outlook.it>
2024-09-11 19:24:18 +0200

    mod

--- src/app/services/actionfigure.service.ts
+++ src/app/services/actionfigure.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ActionfigureService {
-  private apiUrl = 'http://localhost:3000/action-figures'; // URL corretto del server Node.js
-
+  // private apiUrl = 'http://localhost:3000/action-figures'; 
+private apiUrl = 'https://back-end-omega-red.vercel.app/action-figures'
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
